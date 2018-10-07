915d55bd0048460e0b23edfaa565c40a6db1e209
Xinhao Xu <titanxxh@gmail.com>
2018-10-07 23:18:05 +0800

    clear

---
+++ dist/src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var _game = require('./game');
+
+console.log(_game.Agricola);
+//# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import { Agricola } from './game';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+console.log(Agricola);
